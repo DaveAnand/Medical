c0b465b42798af74f9a3ec9b96158d7b08dde7f7
DaveAnand <anand@columbus.rr.com>
2014-06-16 13:54:35 -0400

    use const

--- PreCursor/Program.cs
+++ PreCursor/Program.cs
@@ -10,14 +10,16 @@ namespace PreCursor
             const int indexStart = 1;
             const int indexEnd = 100;
 
-          
+            const int modern = 3;
+            const int medical = 5;
+
             for (int i = indexStart; i <= indexEnd; i++)
             {
                 //If the number is divisible by 3
-                if ((i % 3) == 0)
+                if ((i % modern) == 0)
                 {
                     //If the number is divisible by both 3 and 5, output  ModernMedical
-                    if (i % 5 == 0)
+                    if (i % medical == 0)
                         Console.WriteLine("ModernMedical");
                     else
                         // is only divisible by 3, output Modern
@@ -25,7 +27,7 @@ namespace PreCursor
                 }
 
                 //If the number is divisible by 5 only, output Medical
-                else if (i % 5 == 0)
+                else if (i % medical == 0)
                     Console.WriteLine("Medical");
                 else
                     Console.WriteLine(i);
